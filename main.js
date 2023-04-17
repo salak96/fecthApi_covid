a198f4bd1e0c97566e8bb6a0a1eb97ec23036519
lambang <sasangkakurniawan.com>
2023-04-17 15:23:45 +0700

    addds

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const totalCasesEl = document.getElementById('total-cases');
 const totalDeathsEl = document.getElementById('total-deaths');
 const totalRecoveredEl = document.getElementById('total-recovered');
+// search
+const searchEl = document.getElementById('search');
+const searchBtn = document.getElementById('search-btn');
+const searchResultEl = document.getElementById('search-result');
+
 
 fetch('https://api.covid19api.com/summary')
   .then(response => response.json())
@@ -10,9 +15,43 @@ fetch('https://api.covid19api.com/summary')
     const totalDeaths = globalData.TotalDeaths;
     const totalRecovered = globalData.TotalRecovered;
 
-    totalCasesEl.textContent = totalCases.toLocaleString();
-    totalDeathsEl.textContent = totalDeaths.toLocaleString();
-    totalRecoveredEl.textContent = totalRecovered.toLocaleString();
+    totalCasesEl.innerHTML = totalCases.toLocaleString();
+    totalDeathsEl.innerHTML = totalDeaths.toLocaleString();
+    totalRecoveredEl.innerHTML = totalRecovered.toLocaleString();
+    
+    let countries = [];
+    
+    data.Countries.forEach(country => {
+        countries.push(country.Country);
+    });
+    searchBtn.addEventListener('click', () => {
+        searchResultEl.innerHTML = '';
+        const searchValue = searchEl.value;
+        if (searchValue === '') {
+            alert('Please enter a country name');
+        } else {
+            const searchResult = data.Countries.filter(country => {
+                return country.Country.toLowerCase().includes(searchValue.toLowerCase());
+            });
+            if (searchResult.length === 0) {
+                alert('No result found');
+            } else {
+                searchResult.forEach(country => {
+                    const countryEl = document.createElement('div');
+                    countryEl.classList.add('country');
+                    countryEl.innerHTML = `
+                        <h3>${country.Country}</h3>
+                        <p>Total Confirmed: ${country.TotalConfirmed}</p>
+                        <p>Total Deaths: ${country.TotalDeaths}</p>
+                        <p>Total Recovered: ${country.TotalRecovered}</p>
+                    `;
+                    searchResultEl.appendChild(countryEl);
+                });
+            }
+        }
+    });
+
+
   })
   .catch(error => console.error(error));
 
